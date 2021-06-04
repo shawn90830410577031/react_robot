c9cb95533283602a90ce4725e98724610a601729
shawn90830410577031 <33082378+shawn90830410577031@users.noreply.github.com>
2021-06-04 15:47:28 +0800

    Add files via upload

---
+++ src/robot.js
@@ -0,0 +1,70 @@
+//import { getByDisplayValue } from "@testing-library/dom"
+
+export const robots=[
+
+{
+    id:1,
+    name:'Leanne Graham',
+    username:'Bret',
+    email:'Sincere@april.biz'
+
+},
+{
+    id:2,
+    name:'Ervin Howell',
+    username:'Antonette',
+    email:'Shanna@melissa.tw'
+},
+{
+    id:3,
+    name:'Clememtie Bauch',
+    username:'Samanthia',
+    email:'Nathan@yesenia.net'
+},
+{
+    id:4,
+    name:'Patricia Lebsack',
+    username:'Karianne',
+    email:'Julianne.oconner@kory.org'
+
+},
+{
+    id:5,
+    name:'Chelesey Diatrich',
+    username:'Jarenam',
+    email:'Lucio_Hettinger@annie.ca'
+},
+{
+    id:6,
+    name:'Mrs.Dennis sd',
+    username:'Karley_Datcjer.info',
+    email:'shahwjwjkwjk@rosamong.me'
+},
+{
+    id:7,
+    name:'Kurtis Weisatt',
+    username:'Eliyn Skilies',
+    email:'TellyHoerhjhdj@getByDisplayValue.biz'
+},
+{
+    id:8,
+    name:'nICHOLAS rUSTYATSSLLC v',
+    username:'mAXIMINE_HHDJHDJD',
+    email:'SAGEUEIUHU@ROSAMOND.ME'
+
+},
+{
+    id:9,
+    name:'',
+    username:'',
+    email:''
+},
+{
+    id:10,
+    name:'',
+    username:'',
+    email:''
+}
+
+
+]
